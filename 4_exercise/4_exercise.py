4b7d6cc5306f32858b8d6d0162b64f0ef8ee2c86
GuiZaZotte <enzo.pasquet05@gmail.com>
2025-10-16 11:16:54 +0200

    Bases of the lists

---
+++ 4_exercise/4_exercise.py
@@ -0,0 +1,8 @@
+fruits = ["pomme", "banane", "orange"]
+print(f"Voici la liste fruits : {fruits}.")
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1] = "ananas"
+print(f"La longueur de la liste fruits est de {len(fruits)}.")
+fruits.sort()
+print(f"Voici la liste fruits : {fruits}.")
